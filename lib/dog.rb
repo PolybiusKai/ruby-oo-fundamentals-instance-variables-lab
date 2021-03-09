d4942b071507711bbda266f62b18eade653effc2
rsmith <legendary.belief@gmail.com>
2021-03-09 14:40:48 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
     def name=(dog_name)
-        #This is an instance variable
+        #This is an instance variable and not a local variable
         @this_dogs_name = dog_name
       end
     
